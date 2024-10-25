db9113f0363ab8e6a53423e230a9ce3428c65bde
NoSamuel <25zjin@derryfield.org>
2024-10-25 19:18:41 -0400

    Update PartA.java

--- AT unit 02 Challenge Problems/PartA.java
+++ AT unit 02 Challenge Problems/PartA.java
@@ -1,32 +1,16 @@
+/*
+ * Sam Jin
+ * AT Java
+ */
 import java.util.Scanner;
+
 public class PartA {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Is today Monday? (Please respond YES/NO)");
-        String day = in.nextLine();
-        System.out.println("Please enter your age:");
-        int age = in.nextInt();
-        double discount = 0.0;
-        System.out.println("what is the cost of your meal?");
-        Double cost = in.nextDouble();
-        if (day.toUpperCase().equals("NO")){
-            if (age < 13){
-            discount = discount + 5;    
-            } else if (age >= 50){
-                discount = discount + 7.5;
-            } else {
-                discount = discount;
-            }
-        } else {
-            if (age < 13){
-                discount = discount + 7.5;
-            } else if (age >= 50) {
-                discount = discount + 15;
-            } else {
-                discount = discount + 5;
-            }
-        }
-        double final_cost = cost - cost * (discount / 100);
-        System.out.printf("The final cost of your meal after a %.2f%% discount is $%.2f%n", discount, final_cost);
+        System.out.print("Enter a price in decimal format (21.74): ");
+        double price = in.nextDouble();
+        int dollars = (int) price;
+        int cents = (int) Math.round((price - dollars) * 100) ;
+        System.out.println("The input contains "+ dollars + "dollars and " + cents + "cents.");
     }
 }
