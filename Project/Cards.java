91b178c3284b804b117752709b6d6ea787d8484c
NoSamuel <25zjin@derryfield.org>
2024-11-15 22:54:40 -0500

    Hw 12

--- Project/Cards.java
+++ Project/Cards.java
@@ -1,3 +1,16 @@
+import java.util.Random;
 public class Cards{
-    public enum 
+    //define enumerations
+    public enum card_suit{
+        DIAMOND, SPADE, HEART, CLUB
+    }
+
+    public enum card_rank{
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    //Declare objects
+    private card_suit Suit;
+    private card_rank Rank;
+    
 }
