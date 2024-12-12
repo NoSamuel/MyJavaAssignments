ca2b14f8d03bed3211acef4abbc8c7082aa15c35
NoSamuel <25zjin@derryfield.org>
2024-12-12 08:22:56 -0500

    hw17

--- Hw17/PartA.java
+++ Hw17/PartA.java
@@ -1,6 +1,19 @@
 public class PartA{
     public static void main(String[] args) {
         int[][] values = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
+        };
+        int[][] values_two = {
+            {0, 0},
+            {0, 0},
+            {0, 0},
+            {0, 0},
+            {0, 0},
+        };
+        int[][] values_three = {
             {8, 6, 7},
             {5, 3, 0},
         };
--- Hw17/PartB.java
+++ Hw17/PartB.java
@@ -1,5 +1,28 @@
 public class PartB {
+    public static void print(int[][] values){
+        for (int row = 0; row < values.length; row++){
+            for (int col = 0; col < values.length; col++){
+                System.out.print(values[row][col]);
+                if (col < values[0].length - 1){
+                    System.out.print(",");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        int[][] array_one = {
+            {8, 6, 7},
+            {5, 3, 0},
+        };
+        int[][] array_two = {
+            {0, 7, 6, 5},
+            {2, 2, 0, 1},
+            {8, 4, 5, 7},
+        };
+
+        print(array_one);
+        print(array_two);
+
     }
 }
