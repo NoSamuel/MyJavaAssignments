5cdce6294685eae281ba2453d9f45dfa236e686f
NoSamuel <25zjin@derryfield.org>
2025-01-10 09:02:41 -0500

    hw21

---
+++ Hw21/PartA.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PartA {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("Top5Unemployment.t");
+        Scanner in = new Scanner(inputFile);
+
+        
+    }
+}
