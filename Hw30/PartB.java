32ca09596c431e1d7f815d6fa07717a2253b72da
NoSamuel <25zjin@derryfield.org>
2025-02-10 16:26:57 -0500

    Create PartB.java

---
+++ Hw30/PartB.java
@@ -0,0 +1,23 @@
+class Senior {
+    private String name;
+    private int modelNumber;
+
+    //Constructor
+    public Senior(String name, int modelNumber) {
+        this.name = name;
+        this.modelNumber = modelNumber;
+    }
+
+    //methods
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setModelNumber(int newModelNumber) {
+        modelNumber = newModelNumber;
+    }
+
+    public void recordMeasurement() {
+        
+    }
+}
