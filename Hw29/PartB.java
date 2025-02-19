540b9e8b5b7687b4a99559f4378caf3fb3b87c06
NoSamuel <25zjin@derryfield.org>
2025-02-18 21:43:05 -0500

    finished hw29

---
+++ Hw29/PartB.java
@@ -0,0 +1,47 @@
+class Employee {
+    private String name;
+    private double salary;
+
+    /*
+     * Constructor.
+     */
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        salary = newSalary;
+    }
+
+    public double getAnnualIncome() {
+        // Employees only get an annual salary.
+        return getSalary();
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+    //Constructor
+    public Manager (String name, double salary, double bonus){
+        super(name, salary);
+        this.bonus = bonus;
+    }
+
+    public double getAnnualIncome() {
+        double newincome = super.getAnnualIncome() + bonus;
+        return newincome;
+    }
+}
